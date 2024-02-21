a54dc3949355609a61616a0c395235b8a2b09032
dninosores <dninosores@gmail.com>
2024-02-20 22:13:30 -0800

    Finished UI layout

--- submit-smedge-render.py
+++ submit-smedge-render.py
@@ -1,6 +1,7 @@
 import pymel.core as pm
 import os.path as Path
 import maya.cmds as cmds
+from typing import Callable
 
 
 class RenderLayer:
@@ -20,7 +21,7 @@ class SubmitUIState:
     force_tx: bool = True
     network_project_location: Path = None
     network_render_location: Path = None
-    exclude_directories: list[str]
+    exclude_directories: list[str] = ["autosave", "incrementalSave", "images"]
     start_frame: int = 0
     end_frame: int = 0
 
@@ -46,9 +47,14 @@ class SubmitUI:
     WINDOW_ID = "dninoSmedgeSubmit"
     LABEL_WIDTH = 200
     TICKBOX_WIDTH = 30
+    MARGIN=4
+    apply_state_to_ui: Callable[[SubmitUIState], None]
 
     def __init__(self, state: SubmitUIState):
         self.state = state
+        self.build_ui()
+
+    def build_ui(self):
         if pm.window(self.WINDOW_ID, exists=True):
             cmds.deleteUI(self.WINDOW_ID, window=True)
 
@@ -58,7 +64,12 @@ class SubmitUI:
             resizeToFitChildren=True,
         )
         cmds.setUITemplate("OptionsTemplate", pushTemplate=True)
-        main_layout = pm.columnLayout(parent=self.main_window, adjustableColumn=True)
+        main_layout_form = pm.formLayout(parent=self.main_window)
+        main_layout = pm.columnLayout(parent=main_layout_form, adjustableColumn=True)
+        pm.formLayout(main_layout_form, edit=True, attachForm=[[main_layout, "left", self.MARGIN], 
+                                                               [main_layout, "right", self.MARGIN], 
+                                                               [main_layout, "top", self.MARGIN],
+                                                               [main_layout, "bottom", self.MARGIN]])
         render_options_frame = pm.frameLayout(
             "Render Options",
             collapsable=False,
@@ -71,11 +82,18 @@ class SubmitUI:
             label="Generate TX Files",
             columnWidth=[1, self.LABEL_WIDTH],
             parent=render_options_layout,
+            annotation=(
+                "(Arnold) Whether TX files should be generated. Turn off "
+                "if there are multiple render nodes, as multiple machines generating "
+                "TX files at once can cause conflicts"
+            ),
         )
         force_tx_check = pm.checkBoxGrp(
             label="Force TX Files",
             columnWidth=[1, self.LABEL_WIDTH],
             parent=render_options_layout,
+            annotation=("(Arnold) Errors if TX files are missing. Generate "
+                        "TX files in Arnold>Utilities>TX Manager")
         )
         frame_range_row = pm.rowLayout(
             numberOfColumns=4,
@@ -87,7 +105,7 @@ class SubmitUI:
         end_frame_field = pm.textField(width=30, parent=frame_range_row)
 
         render_layers_frame = pm.frameLayout(
-            "Render Layers",
+            "Enabled Render Layers",
             collapsable=False,
             marginHeight=5,
             parent=render_options_layout,
@@ -123,8 +141,27 @@ class SubmitUI:
             label="Exclude Directories",
             columnWidth=[1, self.LABEL_WIDTH],
             parent=output_options_layout,
+            annotation=("Comma separated list of directories to exclude when "
+                        "syncing project to network directory.")
         )
+        confirm_buttons_form = pm.formLayout(parent=main_layout)
+        close_button = pm.button(label="Close", parent=confirm_buttons_form)
+        generate_config = pm.button(label="Generate Config and Sync", parent=confirm_buttons_form)
+        pm.formLayout(confirm_buttons_form, edit=True, attachForm=[[close_button,"left", 0], 
+                                                                   [close_button, "top", self.MARGIN], 
+                                                                   [generate_config, "right", 0],
+                                                                   [generate_config, "top", self.MARGIN]],
+                                                        attachPosition=[[close_button, "right", self.MARGIN, 50],
+                                                                        [generate_config, "left", self.MARGIN, 50]])
 
+        def apply_state_to_ui(state: SubmitUIState):
+            
+            pass
+
+        self.apply_state_to_ui = apply_state_to_ui
+            
+
+        
     def createFilepathUI(self, label, parent):
         filepath_row_layout = pm.rowLayout(
             numberOfColumns=3,
